68847398f9d8faf3876aa8e334689d94dd71d280
Huhaage <elougone@gmail.com>
2022-05-18 15:36:16 +0200

    fourth commit

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Training } from '../model/training.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  addTraining(training:Training){
+      
+  }
+
+  constructor() { }
+}
