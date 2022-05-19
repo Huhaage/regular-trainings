40205f2d04d94f67cf14fd39666a1a5ff7c56938
Huhaage <elougone@gmail.com>
2022-05-19 09:50:04 +0200

    fifth commit

--- src/app/components/trainings/trainings.component.ts
+++ src/app/components/trainings/trainings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Training } from 'src/app/model/training.model';
+import { CartService } from 'src/app/services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trainings',
@@ -9,13 +10,21 @@ import { Training } from 'src/app/model/training.model';
 })
 export class TrainingsComponent implements OnInit {
   listTrainings : Training[] | undefined;
-  constructor() { }
+  constructor(private cartService : CartService){}
+  onAddToCart(training:Training){
+    this.cartService.addTraining(training);
+  }
 
   ngOnInit(): void {
     this.listTrainings = [
-      {id:1,name:'Java',description:'Formation Java SE  sur 5 jours',price:1500},
-      {id:2,name:'DotNet',description:'Formation DotNet 3 jours',price:1500},
-      {id:2,name:'Python',description:'Formation Python/Django 5 jours',price:1500}
+      {id:1,name:'Java',description:'Formation Java SE  sur 5 jours',price:1500,quantity:1},
+      {id:2,name:'DotNet',description:'Formation DotNet 3 jours',price:1500,quantity:1},
+      {id:2,name:'Python',description:'Formation Python/Django 5 jours',price:1500,quantity:1}
     ];
   }
+
+  // onAddToCart(training:Training){
+  //   console.log(training.name)
+  //   console.log(training.quantity);
+  // }
 }
